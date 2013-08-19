7eca7d94f6890dc2d97a7ed356e0061a7f556d77
a4tech <Dev1@dev>
2013-08-19 10:25:30 +0200

    [EGAMI] some changes and updtes

--- recipes-core/initscripts/initscripts-1.0/egami/hostname.sh
+++ recipes-core/initscripts/initscripts-1.0/egami/hostname.sh
@@ -10,11 +10,38 @@
 
 if test -f /etc/hostname
 then
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-9000ru' ]; then
+		echo "hdx-9000" > /etc/hostname
+	fi
 	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000ru' ]; then
 		echo "hdx-5000" > /etc/hostname
 	fi
 	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000ru' ]; then
-		echo "hdx-1000" > /etc/hostname
-	fi	
+		echo "hde-1000" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ]; then
+		echo "mbtwin" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ]; then
+		echo "mbmini" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000de' ]; then
+		echo "xpeedlx" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-7012' ]; then
+		echo "unibox-hd3" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-7000' ]; then
+		echo "unibox-hd3" > /etc/hostname
+	fi		
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000' ]; then
+		echo "unibox-hd2" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-3000' ]; then
+		echo "unibox-hd1" > /etc/hostname
+	fi
+	if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000' ]; then
+		echo "unibox-hde" > /etc/hostname
+	fi
 	hostname -F /etc/hostname
 fi
