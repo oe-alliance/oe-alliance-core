49560bc7b35dc8aa05cb641a378cf3ea007b633b
satinfo <satinfo76@web.de>
2014-04-27 11:40:18 +0200

    cleanup dead distro

--- recipes-distros/openpl/bootlogo/files/bootlogo.sh
+++
@@ -1,4 +0,0 @@
-# thoug not actually a bootlogo task, set the correct videomode before showing the bootlogo
-cat /etc/videomode > /proc/stb/video/videomode
-
-[ -e /etc/dropbear/dropbear_rsa_host_key ] &&  /usr/bin/showiframe /usr/share/bootlogo.mvi || /usr/bin/showiframe /usr/share/bootlogo_wait.mvi
