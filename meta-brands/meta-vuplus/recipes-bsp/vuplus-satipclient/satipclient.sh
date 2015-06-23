d71a33d71dd8e6cab8736bc643a01b8e5d2e6f6c
Captain <captain.onboard@web.de>
2015-06-23 22:54:15 +0200

    [vuplus] add close source satip client

---
+++ meta-brands/meta-vuplus/recipes-bsp/vuplus-satipclient/satipclient.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if ! [ -x /usr/bin/satipclient ]; then
+	exit 0
+fi
+
+case "$1" in
+	start)
+		start-stop-daemon -S -b -x /usr/bin/satipclient
+		;;
+	stop)
+		start-stop-daemon -K -x /usr/bin/satipclient
+		;;
+	restart|reload)
+		$0 stop
+		$0 start
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+		;;
+esac
+
+exit 0
