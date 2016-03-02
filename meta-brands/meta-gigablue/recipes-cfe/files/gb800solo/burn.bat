1e4c6fe4649052458a526b6b990fbbd49e2c824c
arn354 <arn354@email.de>
2016-03-02 17:39:32 +0100

    [GigaBlue] change burn.bat gb800solo on request of Impex

--- meta-brands/meta-gigablue/recipes-cfe/files/gb800solo/burn.bat
+++ meta-brands/meta-gigablue/recipes-cfe/files/gb800solo/burn.bat
@@ -1,4 +1,4 @@
 flash -noheader usbdisk0:gigablue/solo/kernel.bin flash0.kernel 
 flash -noheader usbdisk0:gigablue/solo/rootfs.bin flash0.rootfs 
-setenv -p STARTUP "boot -z -elf flash0.kernel: 'rootfstype=jffs2 bmem=106M@150M root=/dev/mtdblock6 rw '"  
-
+flash -noheader usbdisk0:gigablue/solo/imageversion flash0.splash 
+setenv -p STARTUP "boot -z -elf flash0.kernel: 'rootfstype=jffs2 bmem=106M@150M root=/dev/mtdblock6 rw '"
