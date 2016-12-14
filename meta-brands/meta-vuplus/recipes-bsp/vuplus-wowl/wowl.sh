132021e7f41a34bd169628a381a3241e5557b0bc
rossi2000 <rossi_2000@hotmail.com>
2016-12-14 16:55:45 +0000

    [Ultimo4k] fix typo in wowl.sh

--- meta-brands/meta-vuplus/recipes-bsp/vuplus-wowl/wowl.sh
+++ meta-brands/meta-vuplus/recipes-bsp/vuplus-wowl/wowl.sh
@@ -11,14 +11,14 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 grep -q 'enable' /proc/stb/fp/wol
 if [ $? -eq 0 ]
 then
-    echo [WOWL] Activaing WOWL...
+	echo [WOWL] Activating WOWL...
 	wl wowl 1
 	wl wowl_activate
 	mknod /dev/wake0 c 34 0
 	echo enable > /proc/stb/fp/forcewol
 	sleep	1
 else
-    echo [WOL] WakeOnLAN is not enabled
+	echo [WOL] WakeOnLAN is not enabled
 fi
 
 
