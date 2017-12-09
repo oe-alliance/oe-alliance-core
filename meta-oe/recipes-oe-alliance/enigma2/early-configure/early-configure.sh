e6cadfd1faadc851e3c9fd4791c4fbe1860e0f5c
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-12-09 17:38:14 +0100

    [early-configure] Add LSB headers

--- meta-oe/recipes-oe-alliance/enigma2/early-configure/early-configure.sh
+++ meta-oe/recipes-oe-alliance/enigma2/early-configure/early-configure.sh
@@ -1,3 +1,18 @@
+#!/bin/sh
+#
+### BEGIN INIT INFO
+# Provides:          early-configure
+# Required-Start:    $local_fs
+# Required-Stop:
+# Should-Start:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Wait for network file systems to be mounted
+# Description:       Network file systems are mounted by
+#                    /etc/network/if-up.d/mountnfs in the background
+#                    when interfaces are brought up; this script waits
+#                    for them to be mounted before carrying on.
+### END INIT INFO
 # Script to fix some configure package getting run too late
 opkg configure update-modules
 
