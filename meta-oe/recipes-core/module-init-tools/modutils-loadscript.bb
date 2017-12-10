b6d47e858f9461c063023b0dd7fb3133bbb6f200
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-12-10 20:52:02 +0100

    [modload] Add LSB header

--- meta-oe/recipes-core/module-init-tools/modutils-loadscript.bb
+++ meta-oe/recipes-core/module-init-tools/modutils-loadscript.bb
@@ -1,9 +1,9 @@
 SECTION = "base"
 SUMMARY = "modutils configuration files"
-LICENSE = "GPLv2"
-LIC_FILES_CHKSUM = "file://modload.sh;beginline=2;endline=2;md5=3b6e5b2caf81c241a5956ed7691327ab"
 SRC_URI = "file://modload.sh"
 PV = "2"
+
+require conf/license/license-gplv2.inc
 
 INITSCRIPT_NAME = "modload.sh"
 INITSCRIPT_PARAMS = "start 5 S ."
--- meta-oe/recipes-core/module-init-tools/modutils-loadscript/modload.sh
+++ meta-oe/recipes-core/module-init-tools/modutils-loadscript/modload.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
-# Licensed under GPLv2
-# Load kernel modules for hotpluggable devices
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          devprobe
+# Required-Start:    kmod checkfs checkroot-bootclean
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Probe USB/HID/input modules
+# Description:       Probes all USB/HID/input modules so that drivers load automatically
+### END INIT INFO
 
-# Probe all USB/HID/input modules so that drivers load automatically
 grep -h '^usb:\|^input:\|^hid:' `find /sys/devices/ -name modalias` | while read m
 do
 	modprobe $m > /dev/null 2> /dev/null
