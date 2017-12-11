2ad745361c82788b3d0c1a288128f719480c2780
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-12-11 04:34:21 +0100

    [volatile-media] Add LSB header

--- meta-oe/recipes-oe-alliance/volatile-media/volatile-media/volatile-media.sh
+++ meta-oe/recipes-oe-alliance/volatile-media/volatile-media/volatile-media.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          media
+# Required-Start:    checkfs checkroot-bootclean
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# X-Start-Before:    mountall
+# Short-Description: Mounts and populates /media
+# Description:       Mounts a tmpfs over /media and
+#                    creates mount points according
+#                    to /etc/fstab entries.
+### END INIT INFO
 
 mountpoint -q "/media" || mount -t tmpfs -o size=64k tmpfs /media
 
