e39f7feb2879b0193980e84426b17a19610ef894
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-12-16 18:37:54 +0100

    [vuplus-shutdown] Add LSB header

--- meta-brands/meta-vuplus/recipes-bsp/vuplus-shutdown/vuplus-shutdown.sh
+++ meta-brands/meta-vuplus/recipes-bsp/vuplus-shutdown/vuplus-shutdown.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/sh -e
+### BEGIN INIT INFO
+# Provides:          brcm-shutdown
+# Required-Start:    
+# Required-Stop:     
+# Should-Start:      
+# Should-Stop:       halt
+# X-Stop-After:      umountroot
+# Default-Start:     
+# Default-Stop:      0
+# Short-Description: Shutdown the STB.
+# Description:       Shutdown the STB.
+### END INIT INFO
 
 /usr/bin/turnoff_power
 
