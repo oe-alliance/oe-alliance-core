9cbe858a12b52f37ba46802aa9c1ce11c277fad1
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-12-23 11:19:27 +0100

    [early-configure] Cleanup

--- meta-oe/recipes-oe-alliance/enigma2/early-configure.bb
+++ meta-oe/recipes-oe-alliance/enigma2/early-configure.bb
@@ -7,9 +7,14 @@ require conf/license/license-gplv2.inc
 
 SRC_URI = "file://${PN}.sh"
 
+inherit update-rc.d
+
+INITSCRIPT_NAME = "${PN}.sh"
+INITSCRIPT_PARAMS = "start 15 S ."
+
 S = "${WORKDIR}"
 
 do_install() {
-    install -d ${D}/etc/rcS.d
-    install -m 0755 ${WORKDIR}/${PN}.sh ${D}/etc/rcS.d/S15${PN}.sh
+    install -d ${D}/etc/init.d
+    install -m 0755 ${WORKDIR}/${PN}.sh ${D}/etc/init.d/${PN}.sh
 }
--- meta-oe/recipes-oe-alliance/enigma2/early-configure/early-configure.sh
+++ meta-oe/recipes-oe-alliance/enigma2/early-configure/early-configure.sh
@@ -2,19 +2,15 @@
 #
 ### BEGIN INIT INFO
 # Provides:          early-configure
-# Required-Start:    $local_fs
+# Required-Start:    checkroot-volatile
 # Required-Stop:
-# Should-Start:
 # Default-Start:     S
 # Default-Stop:
-# Short-Description: Wait for network file systems to be mounted
-# Description:       Network file systems are mounted by
-#                    /etc/network/if-up.d/mountnfs in the background
-#                    when interfaces are brought up; this script waits
-#                    for them to be mounted before carrying on.
+# Short-Description: Script to fix some configures getting run too late
+# Description:       Script to fix some configures getting run too late
 ### END INIT INFO
-# Script to fix some configure package getting run too late
+
 opkg configure update-modules
 
 # suicide
-rm -f /etc/rcS.d/S*early-configure.sh
+rm -f /etc/rcS.d/S*early-configure.sh /etc/init.d/S*early-configure.sh
