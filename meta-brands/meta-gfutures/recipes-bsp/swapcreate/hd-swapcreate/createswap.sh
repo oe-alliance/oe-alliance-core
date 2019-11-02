526b7945c9644cb3faabfa8accf11f353d883c14
captain <captain.onboard@web.de>
2019-11-02 09:42:48 +0100

    enable swap again for hd51/h7/8100s/vs1500

--- meta-brands/meta-gfutures/recipes-bsp/swapcreate/hd-swapcreate/createswap.sh
+++ meta-brands/meta-gfutures/recipes-bsp/swapcreate/hd-swapcreate/createswap.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
+if [ ! "$(grep config.misc.firstrun /etc/enigma2/settings)" == "config.misc.firstrun=false" ]; then
+	echo w | fdisk /dev/mmcblk0 
+	partprobe 
+fi
+
+
+
 device1=$(blkid -t  PARTLABEL="swap" -o device | head -n1)
-swap1=$(cat /proc/swaps | grep -o "/dev/mmcblk0p10" | head -n1)
+### /dev/mmcblk0p6 (2 partition) or /dev/mmcblk0p10 (4 partition) or /dev/mmcblk0p7 (new system)
+swap1=$(cat /proc/swaps | grep -o "$device1" | head -n1)
 
 if [ "$device1" == "" ]; then
 	echo "  Sorry, no swap drive found"
 	exit 1
 fi
 
-grep -q "/dev/mmcblk0p10" /etc/fstab
+grep -q "$device1" /etc/fstab
 
 if [ $? -ne 0 ]; then
-	mkswap /dev/mmcblk0p10
-	swapon /dev/mmcblk0p10
-	echo '/dev/mmcblk0p10 none swap defaults 0 0' >> /etc/fstab
+	mkswap $device1
+	swapon $device1
+	echo "$device1 none swap defaults 0 0" >> /etc/fstab
 elif [ "$swap1" == "" ]; then
-	mkswap /dev/mmcblk0p10
-	swapon /dev/mmcblk0p10
+	mkswap $device1
+	swapon $device1
 fi
+
