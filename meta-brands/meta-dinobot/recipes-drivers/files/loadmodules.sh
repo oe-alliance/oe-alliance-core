db579133762d0163f48472ac6e6b6cd2f0f581c9
Captain <captain.onboard@web.de>
2022-04-17 09:48:51 +0200

    [dinobot] switch to enigma.info to read the imagedir

--- meta-brands/meta-dinobot/recipes-drivers/files/loadmodules.sh
+++ meta-brands/meta-dinobot/recipes-drivers/files/loadmodules.sh
@@ -5,7 +5,7 @@ cd /lib/modules/4.4.35/extra/
 ./load
 
 if [ -e /proc/stb/info/upd_path ]; then
-  upd_path=`cat /etc/image-version |grep imagedir= | cut -d= -f2`
+  upd_path=`cat /usr/lib/enigma.info |grep imagedir= | cut -d= -f2`
   if [ ! -z $upd_path ]; then
     echo "/$upd_path" > /proc/stb/info/upd_path
   fi
