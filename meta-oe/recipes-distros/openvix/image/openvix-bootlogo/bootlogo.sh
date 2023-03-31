8e07d15076e43ae815ad89ac7d992e9d2f6f9de4
Rowland Creed <rowland@moralet.com>
2023-04-01 01:09:12 +0200

    [openvix] bootlogo priorities Update to match priorities in Directories.py

--- meta-oe/recipes-distros/openvix/image/openvix-bootlogo/bootlogo.sh
+++ meta-oe/recipes-distros/openvix/image/openvix-bootlogo/bootlogo.sh
@@ -4,18 +4,22 @@
 # and set the correct videomode before showing the bootlogo
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
-if [ -f /etc/enigma2/bootlogo.mvi ]; then 
-	/usr/bin/showiframe /etc/enigma2/bootlogo.mvi
-else
-	skin=`sed -En 's|config\.skin\.primary_skin=(.+)/skin\.xml|\1|p' /etc/enigma2/settings`
-	if [ z $skin ]; then
-		skin=`strings -n 10 /usr/lib/enigma2/python/skin.pyo | egrep -o -m 1 ".+/skin.xml" | sed 's|/skin.xml.*||'`
+if [ -x /usr/bin/showiframe ]; then
+	SKIN=`sed -En 's|config\.skin\.primary_skin=(.+)/skin\.xml|\1|p' /etc/enigma2/enigma2/settings`
+	if [ -z "$SKIN" ]; then
+		SKIN=`strings -n 10 /usr/lib/enigma2/python/skin.pyc | egrep -o -m 1 ".+/skin.xml" | sed 's|/skin.xml.*||'`
 	fi
-	if [ -n $skin -a -f /usr/share/enigma2/$skin/bootlogo.mvi ]; then
-		/usr/bin/showiframe /usr/share/enigma2/$skin/bootlogo.mvi
+	if [ -n "${SKIN}" ]; then
+		SEARCHDIRS="/etc/enigma2/${SKIN} /etc/enigma2/skin_common /etc/enigma2 /usr/share/enigma2/${SKIN} /usr/share/enigma2/skin_default /usr/share/enigma2 /usr/share"
 	else
-		/usr/bin/showiframe /usr/share/bootlogo.mvi
-    fi
+		SEARCHDIRS="/etc/enigma2/skin_common /etc/enigma2 /usr/share/enigma2/skin_default /usr/share/enigma2 /usr/share"
+	fi
+	for DIR in ${SEARCHDIRS}; do
+		if [ -d "${DIR}" ] && [ -f "${DIR}/bootlogo.mvi" ]; then
+			/usr/bin/showiframe "${DIR}/bootlogo.mvi" &
+			break
+		fi
+	done
 fi
 
 [ -f /etc/init.d/bootlogo.py ] && /usr/bin/python /etc/init.d/bootlogo.py
