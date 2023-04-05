26b9f0d19b941212bb069c5ca441df627e9296b9
Ev0-BH <Ev0_BhT@outlook.com>
2023-04-05 22:50:53 +0100

    [openbh] bootlogo priorities Update to match priorities in Directories.py

--- meta-oe/recipes-distros/openbh/bootlogo/files/bootlogo.sh
+++ meta-oe/recipes-distros/openbh/bootlogo/files/bootlogo.sh
@@ -3,8 +3,22 @@
 # and set the correct videomode before showing the bootlogo
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
-BOOTLOGO=/usr/share/bootlogo.mvi
-[ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
-/usr/bin/showiframe ${BOOTLOGO}
+if [ -x /usr/bin/showiframe ]; then
+	SKIN=`sed -En 's|config\.skin\.primary_skin=(.+)/skin\.xml|\1|p' /etc/enigma2/enigma2/settings`
+	if [ -z "$SKIN" ]; then
+		SKIN=`strings -n 10 /usr/lib/enigma2/python/skin.pyc | egrep -o -m 1 ".+/skin.xml" | sed 's|/skin.xml.*||'`
+	fi
+	if [ -n "${SKIN}" ]; then
+		SEARCHDIRS="/etc/enigma2/${SKIN} /etc/enigma2/skin_common /etc/enigma2 /usr/share/enigma2/${SKIN} /usr/share/enigma2/skin_default /usr/share/enigma2 /usr/share"
+	else
+		SEARCHDIRS="/etc/enigma2/skin_common /etc/enigma2 /usr/share/enigma2/skin_default /usr/share/enigma2 /usr/share"
+	fi
+	for DIR in ${SEARCHDIRS}; do
+		if [ -d "${DIR}" ] && [ -f "${DIR}/bootlogo.mvi" ]; then
+			/usr/bin/showiframe "${DIR}/bootlogo.mvi" &
+			break
+		fi
+	done
+fi
 
 [ -f /etc/init.d/bootlogo.py ] && /usr/bin/python /etc/init.d/bootlogo.py
