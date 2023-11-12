0463494bbf4f799e79bd50ca27e18b8fd0a8ae66
Captain <captain.onboard@web.de>
2023-11-12 13:51:27 +0100

    dreambox add STARTUP_4 Slot

--- meta-brands/meta-dream/recipes-bsp/dream-gpt-startup/dream-data.sh
+++ meta-brands/meta-dream/recipes-bsp/dream-gpt-startup/dream-data.sh
@@ -7,20 +7,23 @@ while ! is_mounted "/dev/dreambox-data"; do
   sleep 1
 done
 
-if [ ! -f "/data/STARTUP" ]; then
+if [ ! -f "/data/STARTUP_4" ]; then
   cp -f /usr/share/startup/* /data
 fi
 
 input_file="/data/bootconfig.txt"
 temp_file="/tmp/temp_file.txt"
-
-if [ -e "$input_file" ]; then
+chmod 755 /data/bootconfig.txt
+chgrp root /data/bootconfig.txt
+chown root /data/bootconfig.txt
+if [ -e "$input_file" ] && ! grep -q -i 'lock=' "$input_file" || grep -q -i 'lock=0' "$input_file"; then
   count_cmd=$(grep -o 'cmd=' "$input_file" | wc -l)
   if [ "$count_cmd" -le 2 ]; then
   new_content=$(cat <<EOF
 default=0
 details=0
-timeout=2
+timeout=3
+lock=0
 fb_pos=100,400
 fb_size=1080,300
 [BuildIn Slot 1]
@@ -31,6 +34,9 @@ cmd=ext4load mmc 1:6 1080000 /boot/kernel.img;bootm;
 arg=\${bootargs} logo=osd0,loaded,0x7f800000 vout=1080p50hz,enable hdmimode=1080p50hz fb_width=1280 fb_height=720 panel_type=lcd_4
 [BuildIn Slot 3]
 cmd=ext4load mmc 1:7 1080000 /boot/kernel.img;bootm;
+arg=\${bootargs} logo=osd0,loaded,0x7f800000 vout=1080p50hz,enable hdmimode=1080p50hz fb_width=1280 fb_height=720 panel_type=lcd_4
+[BuildIn Slot 4]
+cmd=ext4load mmc 1:8 1080000 /boot/kernel.img;bootm;
 arg=\${bootargs} logo=osd0,loaded,0x7f800000 vout=1080p50hz,enable hdmimode=1080p50hz fb_width=1280 fb_height=720 panel_type=lcd_4
 [   Recovery   ]
 cmd=imgread kernel recovery \${loadaddr} 0; bootm \${loadaddr};
