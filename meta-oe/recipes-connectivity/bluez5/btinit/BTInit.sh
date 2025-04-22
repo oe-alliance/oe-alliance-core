473a4f934dc3ad9a87c02a12629d73556c0aa596
atvcaptain <openatv@gmail.com>
2025-04-22 11:08:17 +0200

    [gigablue] trio4kpro update bt init script again, try to fix timing issue

--- meta-oe/recipes-connectivity/bluez5/btinit/BTInit.sh
+++ meta-oe/recipes-connectivity/bluez5/btinit/BTInit.sh
@@ -49,10 +49,10 @@ fi
 start() {
     log "Starting..."
     
-    if [ "$MODEL" = "gbmv200" ]; then
-        log "Attaching HCI for sprd"
-        hciattach_sprd /dev/ttyBT0 sprd > /var/log/hciattach.log &
-    fi
+    #if [ "$MODEL" = "gbmv200" ]; then
+    #    log "Attaching HCI for sprd"
+    #    hciattach_sprd /dev/ttyBT0 sprd > /var/log/hciattach.log &
+    #fi
 
     hciconfig hci0 up > /dev/null 2>&1 && log "Attaching hci0"
     (hcitool dev | awk 'NR==2 {print $2}' | while read -r BT_MAC; do
